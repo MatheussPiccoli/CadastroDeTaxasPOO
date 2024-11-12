9f335776ceb729d1b62c8905ceadef29395c5219
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-11-12 08:58:16 -0300

    Update main.py
    Adicionei uma função para limpar a tela do terminal de forma compatível com sistemas Linux e Windows. Também incluí uma saudação dinâmica baseada no horário atual (manhã, tarde, noite) para uma interação mais personalizada (será usada mais tarde na interface principal).

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import os
+import datetime
+
+# Função para limpar a tela do terminal em Linux e Windows
+def limpar_tela():
+    if os.name == 'nt':
+        os.system('cls') 
+    else:
+        os.system('clear')
+
+# Saudação por horário
+horario = datetime.datetime.now().hour
+manha = -1 < horario < 12
+tarde = 11 < horario < 18
+noite = 18 < horario < 24
+if manha:
+    saudacao = 'Bom dia'
+elif tarde:
+    saudacao = 'Boa tarde'
+else:
+    saudacao = 'Boa noite'
+
+
+
 
