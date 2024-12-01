1881be22b2035c72f0465bdbf6e2c85241bf1683
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-12-01 14:38:59 -0300

    Update operadora.py

--- operadora.py
+++ operadora.py
@@ -1,7 +1,11 @@
-class Operadora:
-    def __init__(self, nome, taxa):
-        self.nome = nome
-        self.taxa = taxa
+from operadora import Operadora
+
+class Venda(Operadora):
+    def __init__(self, valor_bruto, operadora_nome, operadora_taxa):
+        super().__init__(operadora_nome, operadora_taxa)
+        self.valor_bruto = valor_bruto
+        self.valor_liquido = self.valor_bruto - (self.valor_bruto) * ((self.taxa) / 100)
         
-    def __repr__(self):
-        return f'Operadora(nome={self.nome}, taxa={self.taxa}%)'
+    def __str__(self):
+        return (f"valor_bruto=R${self.valor_bruto:.2f}, taxa={self.taxa}%, valor_liquido=R${self.valor_liquido:.2f}")
+    
